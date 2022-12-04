738d1096c096c8d5c7489f9bb7aa0382874eab1d
Sean Knowles <s@seanknowles.dev>
2022-12-04 19:11:49 +0000

    feat: #20 shaka player added and working

--- apps/clients/web/onvu/src/app/screens/player/index.tsx
+++ apps/clients/web/onvu/src/app/screens/player/index.tsx
@@ -1,10 +1,16 @@
+import ShakaPlayer from 'shaka-player-react';
+import 'shaka-player/dist/controls.css';
+
 /* eslint-disable-next-line */
 export interface PlayerProps {}
 
 export function Player(props: PlayerProps) {
   return (
     <div>
-      <h1>Welcome to Player!</h1>
+      <ShakaPlayer
+        autoPlay
+        src="https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd"
+      />
     </div>
   );
 }
